2f52325b9409f3da7e9ce0d5331cf01ed97fd6e0
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-09-18 21:28:55 +0100

    adding request pretty printing

--- bot_handler.go
+++ bot_handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -12,14 +13,41 @@ func main() {
 }
 
 func botHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "wabalabadubdub!!!")
-
 	r.ParseForm() // Parses the request body
-	challenge := r.PostForm.Get("challenge")
+	//challenge := r.PostForm.Get("challenge")
 	//w.WriteHeader(http.StatusOK)
-	w.Header().Add("Content-Type", "text/plain")
-	fmt.Fprint(w, challenge)
+	//w.Header().Add("Content-Type", "text/plain")
+	//fmt.Fprint(w, challenge)
+	//
+	//fmt.Fprintf(w, "%v\n", r)
+	//
+	fmt.Fprint(w, formatRequest(r))
 
-	fmt.Fprint(w, "\nwabalabadubdub???")
+}
 
+// formatRequest generates ascii representation of a request
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	// Add the host
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+	// Return the request as a string
+	return strings.Join(request, "\n")
 }
